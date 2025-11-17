3b543dfb37524b27798b6a0c8d7cdde544e4c9f9
Maksim <89604017835@mail.ru>
2025-11-17 22:05:22 +0300

    bot update9

--- server/index.js
+++ server/index.js
@@ -22,177 +22,19 @@ const bot = new TelegramApi(token, { polling: true });
 
 
 //=====================================================================
-// // ---- Memory storage (имитация БД) ----
-// function loadUsers() {
-//   return JSON.parse(fs.readFileSync("users.json", "utf-8"));
-// }
-// function saveUsers(data) {
-//   fs.writeFileSync("users.json", JSON.stringify(data, null, 2));
-// }
+// ---- Memory storage (имитация БД) ----
+function loadUsers() {
+  return JSON.parse(fs.readFileSync("users.json", "utf-8"));
+}
+function saveUsers(data) {
+  fs.writeFileSync("users.json", JSON.stringify(data, null, 2));
+}
 
-// // ---- Диалог состояния ----
-// const userState = {};  // chatId → "waiting_city_time"
-
-
-//  /start Команда
-// const againOptions = {
-//   reply_markup: JSON.stringify({
-//     inline_keyboard: [
-//       [{ text: "Запуск приложения", web_app: { url: webAppUrl } }]
-//     ]
-//   })
-// };
+// ---- Диалог состояния ----
+const userState = {};  // chatId → "waiting_city_time"
 
 
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-
-//   bot.sendMessage(chatId, "Привет! Хочешь получать ежедневный прогноз погоды?\n" +
-//     "Используй команду /setcity\n" +
-//     "Или нажми кнопку, чтобы открыть приложение 👇", againOptions);
-// });
-
-// // -------------------------------------
-// //  /setcity — пользователь начинает настройку
-// // -------------------------------------
-// bot.onText(/\/setcity/, (msg) => {
-//   const chatId = msg.chat.id;
-
-//   userState[chatId] = "waiting_city_time";
-
-//   bot.sendMessage(chatId, "Укажите город и время в формате: Москва 09:00");
-// });
-
-// // -------------------------------------
-// //  Обработка ввода "Москва 09:00"
-// // -------------------------------------
-// bot.on("message", async (msg) => {
-//   const chatId = msg.chat.id;
-//   const text = msg.text;
-
-//   if (userState[chatId] !== "waiting_city_time") return;
-
-//   const parsed = text.match(/(.+)\s+(\d{1,2}):(\d{2})/);
-
-//   if (!parsed) {
-//     return bot.sendMessage(chatId, "Формат неверный. Пример: Москва 09:00");
-//   }
-
-//   const city = parsed[1].trim();
-//   const hours = parseInt(parsed[2]);
-//   const minutes = parseInt(parsed[3]);
-
-//   // ---- Получаем timezone города ----
-//   const owUrl = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_BOT_API_KEY}&q=${encodeURIComponent(city)}&days=1&aqi=no&alerts=no&lang=ru`;
-
-//   const resp = await fetch(owUrl);
-//   const data = await resp.json();
-
-//   if (!data.location.name) {
-//     return bot.sendMessage(chatId, "Город не найден.");
-//   }
-
-//   const nowUTC = Math.floor(Date.now() / 1000);
-//   const timezoneOffset = data.location.localtime_epoch - nowUTC;
-
-//   // ---- Конвертация пользовательского времени -> UTC ----
-//   // Время пользователя (локальное по городу) минус смещение
-//   const totalMinutes = hours * 60 + minutes;
-//   const utcMinutes = totalMinutes - timezoneOffset / 60;
-
-//   let utcH = Math.floor((utcMinutes / 60 + 24) % 24);
-//   let utcM = ((utcMinutes % 60) + 60) % 60;
-
-//   // ---- Сохраняем в JSON ----
-//   const users = loadUsers();
-
-//   const entry = {
-//     chatId,
-//     city,
-//     tzOffset: timezoneOffset,
-//     userH: hours,
-//     userM: minutes,
-//     utcH,
-//     utcM
-//   };
-
-//   // удаляем предыдущую настройку
-//   const filtered = users.filter(u => u.chatId !== chatId);
-//   filtered.push(entry);
-//   saveUsers(filtered);
-
-//   delete userState[chatId];
-
-//   bot.sendMessage(chatId,
-//     `Готово!\nБуду отправлять прогноз для *${city}* каждый день в *${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}*`,
-//     { parse_mode: "Markdown" }
-//   );
-// });
-
-// // -------------------------------------
-// //  CRON — Проверка каждую минуту
-// // -------------------------------------
-// cron.schedule("* * * * *", async () => {
-//   const now = new Date();
-//   const h = now.getUTCHours();
-//   const m = now.getUTCMinutes();
-
-//   const users = loadUsers();
-
-//   for (const u of users) {
-//     if (u.utcH === h && u.utcM === m) {
-//       // --- Получаем прогноз ---
-//     //  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(u.city)}&appid=${process.env.WEATHER_BOT_API_KEY}&units=metric&lang=ru`;
-//       const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_BOT_API_KEY}&q=${encodeURIComponent(u.city)}&days=1&aqi=no&alerts=no&lang=ru`;
-
-//       const resp = await fetch(url);
-//       const data = await resp.json();
-
-//       const chanceOfRanin = data.forecast.forecastday[0].day.daily_chance_of_rain;
-//       const chanceOfSnow = data.forecast.forecastday[0].day.daily_chance_of_snow;
-//       const precepitationChance = chanceOfRanin > chanceOfSnow ? `Вероятность дождя: ${chanceOfRanin}%` : `Вероятность выпадения снега: ${chanceOfSnow}%`
-
-//       const text =
-//         `Погода в *${u.city}* сейчас:\n` +
-//         `Температура: *${Math.trunc(data.current.temp_c)}°C*\n` +
-//         `${data.current.condition.text}\n` +
-//         `\n` +
-//         `Погода сегодня:\n` +
-//         `Температура max: ${Math.trunc(data.forecast.forecastday[0].day.maxtemp_c)}\n` +
-//         `Температура min: ${Math.trunc(data.forecast.forecastday[0].day.mintemp_c)}\n` +
-//         `Ветер: ${data.forecast.forecastday[0].day.maxwind_kph}км/ч\n` +
-//         `${precepitationChance}`;
-
-//       bot.sendMessage(u.chatId, text, { parse_mode: "Markdown" });
-//     }
-//   }
-// });
-
-// //=====================================================================
-// // -------------------------------------
-// //  /deletecity — удалить город пользователя
-// // -------------------------------------
-//     bot.onText(/\/deletecity/, (msg) => {
-//       const chatId = msg.chat.id;
-
-//       const users = loadUsers();
-
-//       // проверяем, есть ли запись
-//       const exists = users.some(u => u.chatId === chatId);
-
-//       if (!exists) {
-//         return bot.sendMessage(chatId, "У вас нет сохранённого города.");
-//       }
-
-//       // удаляем запись
-//       const updated = users.filter(u => u.chatId !== chatId);
-//       saveUsers(updated);
-
-//       bot.sendMessage(chatId, "Ваш город был успешно удалён. Ежедневные уведомления отключены.");
-//     });
-
-//====================================================================
-
+ ///start Команда
 const againOptions = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
@@ -202,14 +44,172 @@ const againOptions = {
 };
 
 
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, "Привет! Хочешь получать ежедневный прогноз погоды?\n" +
+    "Используй команду /setcity\n" +
+    "Или нажми кнопку, чтобы открыть приложение 👇", againOptions);
+});
+
+// -------------------------------------
+//  /setcity — пользователь начинает настройку
+// -------------------------------------
+bot.onText(/\/setcity/, (msg) => {
+  const chatId = msg.chat.id;
+
+  userState[chatId] = "waiting_city_time";
+
+  bot.sendMessage(chatId, "Укажите город и время в формате: Москва 09:00");
+});
+
+// -------------------------------------
+//  Обработка ввода "Москва 09:00"
+// -------------------------------------
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text === "/start") {
-    await bot.sendMessage(chatId, "Привет! Нажми кнопку, чтобы открыть приложение 👇", againOptions);
+  if (userState[chatId] !== "waiting_city_time") return;
+
+  const parsed = text.match(/(.+)\s+(\d{1,2}):(\d{2})/);
+
+  if (!parsed) {
+    return bot.sendMessage(chatId, "Формат неверный. Пример: Москва 09:00");
+  }
+
+  const city = parsed[1].trim();
+  const hours = parseInt(parsed[2]);
+  const minutes = parseInt(parsed[3]);
+
+  // ---- Получаем timezone города ----
+  const owUrl = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_BOT_API_KEY}&q=${encodeURIComponent(city)}&days=1&aqi=no&alerts=no&lang=ru`;
+
+  const resp = await fetch(owUrl);
+  const data = await resp.json();
+
+  if (!data.location.name) {
+    return bot.sendMessage(chatId, "Город не найден.");
+  }
+
+  const nowUTC = Math.floor(Date.now() / 1000);
+  const timezoneOffset = data.location.localtime_epoch - nowUTC;
+
+  // ---- Конвертация пользовательского времени -> UTC ----
+  // Время пользователя (локальное по городу) минус смещение
+  const totalMinutes = hours * 60 + minutes;
+  const utcMinutes = totalMinutes - timezoneOffset / 60;
+
+  let utcH = Math.floor((utcMinutes / 60 + 24) % 24);
+  let utcM = ((utcMinutes % 60) + 60) % 60;
+
+  // ---- Сохраняем в JSON ----
+  const users = loadUsers();
+
+  const entry = {
+    chatId,
+    city,
+    tzOffset: timezoneOffset,
+    userH: hours,
+    userM: minutes,
+    utcH,
+    utcM
+  };
+
+  // удаляем предыдущую настройку
+  const filtered = users.filter(u => u.chatId !== chatId);
+  filtered.push(entry);
+  saveUsers(filtered);
+
+  delete userState[chatId];
+
+  bot.sendMessage(chatId,
+    `Готово!\nБуду отправлять прогноз для *${city}* каждый день в *${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}*`,
+    { parse_mode: "Markdown" }
+  );
+});
+
+// -------------------------------------
+//  CRON — Проверка каждую минуту
+// -------------------------------------
+cron.schedule("* * * * *", async () => {
+  const now = new Date();
+  const h = now.getUTCHours();
+  const m = now.getUTCMinutes();
+
+  const users = loadUsers();
+
+  for (const u of users) {
+    if (u.utcH === h && u.utcM === m) {
+      // --- Получаем прогноз ---
+    //  const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(u.city)}&appid=${process.env.WEATHER_BOT_API_KEY}&units=metric&lang=ru`;
+      const url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_BOT_API_KEY}&q=${encodeURIComponent(u.city)}&days=1&aqi=no&alerts=no&lang=ru`;
+
+      const resp = await fetch(url);
+      const data = await resp.json();
+
+      const chanceOfRanin = data.forecast.forecastday[0].day.daily_chance_of_rain;
+      const chanceOfSnow = data.forecast.forecastday[0].day.daily_chance_of_snow;
+      const precepitationChance = chanceOfRanin > chanceOfSnow ? `Вероятность дождя: ${chanceOfRanin}%` : `Вероятность выпадения снега: ${chanceOfSnow}%`
+
+      const text =
+        `Погода в *${u.city}* сейчас:\n` +
+        `Температура: *${Math.trunc(data.current.temp_c)}°C*\n` +
+        `${data.current.condition.text}\n` +
+        `\n` +
+        `Погода сегодня:\n` +
+        `Температура max: ${Math.trunc(data.forecast.forecastday[0].day.maxtemp_c)}\n` +
+        `Температура min: ${Math.trunc(data.forecast.forecastday[0].day.mintemp_c)}\n` +
+        `Ветер: ${data.forecast.forecastday[0].day.maxwind_kph}км/ч\n` +
+        `${precepitationChance}`;
+
+      bot.sendMessage(u.chatId, text, { parse_mode: "Markdown" });
+    }
   }
 });
+
+//=====================================================================
+// -------------------------------------
+//  /deletecity — удалить город пользователя
+// -------------------------------------
+    bot.onText(/\/deletecity/, (msg) => {
+      const chatId = msg.chat.id;
+
+      const users = loadUsers();
+
+      // проверяем, есть ли запись
+      const exists = users.some(u => u.chatId === chatId);
+
+      if (!exists) {
+        return bot.sendMessage(chatId, "У вас нет сохранённого города.");
+      }
+
+      // удаляем запись
+      const updated = users.filter(u => u.chatId !== chatId);
+      saveUsers(updated);
+
+      bot.sendMessage(chatId, "Ваш город был успешно удалён. Ежедневные уведомления отключены.");
+    });
+
+//====================================================================
+
+// const againOptions = {
+//   reply_markup: JSON.stringify({
+//     inline_keyboard: [
+//       [{ text: "Запуск приложения", web_app: { url: webAppUrl } }]
+//     ]
+//   })
+// };
+
+
+// bot.on("message", async (msg) => {
+//   const chatId = msg.chat.id;
+//   const text = msg.text;
+
+//   if (text === "/start") {
+//     await bot.sendMessage(chatId, "Привет! Нажми кнопку, чтобы открыть приложение 👇", againOptions);
+//   }
+// });
 
 // --- API ДЛЯ ФРОНТЕНДА ---
 app.get("/api/weather", async (req, res) => {
